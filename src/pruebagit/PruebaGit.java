98c3e39f79c7ab7de883acce374e3fded521d654
FranyCold <125722058+FranyCold@users.noreply.github.com>
2023-08-29 13:24:19 -0300

    Prueba mas cosas
    Cambios realizados

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -17,6 +17,8 @@ public class PruebaGit {
     public static void main(String[] args) {
         // TODO code application logic here
         int edad = 10;
+        String nombre = "Franco";
+        System.out.println(nombre + " " + edad);
     }
-    
+
 }
